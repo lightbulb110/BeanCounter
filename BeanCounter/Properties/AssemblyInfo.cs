b3231163f3e10158141b37eeea939cb419a40eba
Nathan <anonymous>
2022-04-26 08:48:59 -0400

    documentation

--- BeanCounter/Properties/AssemblyInfo.cs
+++ BeanCounter/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BeanCounter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Open Sourced by NateO")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BeanCounter")]
